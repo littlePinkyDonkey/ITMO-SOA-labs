81a763a875843914e17283626e8343eba533e215
Andrei <teplyh.andre@yandex.ru>
2021-10-21 11:14:46 +0300

    added normal ExtraOperationsServiceImpl

--- servlet-service/src/main/java/service/impl/ExtraOperationsServiceImpl.java
+++ servlet-service/src/main/java/service/impl/ExtraOperationsServiceImpl.java
@@ -52,34 +52,7 @@ public class ExtraOperationsServiceImpl implements ExtraOperationsService {
     public String findMinId() {
         Gson gson = gsonBuilder.create();
 
-        List<Coordinates> coordinates = coordinatesService.getAll();
         List<Dragon> dragons = dragonService.getAll();
-        List<Person> persons = personService.getAll();
-
-        Coordinates coordinate = coordinates.get(0);
-        Dragon dragon = dragons.get(0);
-        Person person = persons.get(0);
-
-        int coordinatesId = Integer.MAX_VALUE;
-        int dragonId = Integer.MAX_VALUE;
-        int personId = Integer.MAX_VALUE;
-
-        if (coordinate != null) {
-            coordinatesId = coordinate.getCoordinatesId();
-        }
-        if (dragon != null) {
-            dragonId = Math.toIntExact(dragon.getId());
-        }
-        if (person != null) {
-            personId = person.getPersonId();
-        }
-
-        if (dragonId < coordinatesId) {
-            return gson.toJson(dragonMapper.entityToDto(dragons.get(0)));
-        } else if (personId < coordinatesId) {
-            return gson.toJson(personMapper.entityToDto(persons.get(0)));
-        } else {
-            return gson.toJson(coordinatesMapper.entityToDto(coordinates.get(0)));
-        }
+        return gson.toJson(dragons.get(0));
     }
 }
